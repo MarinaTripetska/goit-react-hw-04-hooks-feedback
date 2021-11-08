c5805ab24ba72c4f9cd8550e19f2fb7cbe6ce30a
Marina Tripetska <marina.tripetskaya@gmail.com>
2021-11-08 14:34:41 +0100

    Updates

---
+++ static/js/main.f27ff5fc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),i=c.n(o),a=(c(8),c(9),c(0));var s=function(){return Object(a.jsx)(a.Fragment,{children:Object(a.jsx)("h1",{children:"Hollo World!"})})};i.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.f27ff5fc.chunk.js.map
